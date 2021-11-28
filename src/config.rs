db7d96eb57f58c1121065a44b0a6d6abc35385d5
flammenderdrache <laberschleichi@gmail.com>
2021-11-28 22:58:55 +0100

    Added minimal config and database migration

--- src/config.rs
+++ src/config.rs
@@ -1,7 +1,24 @@
-struct Config {
-	database_url: String,
+use crate::Error;
+use std::fs::File;
+use std::io::Read;
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+pub struct Config {
+	pub database: Database,
+}
+
+#[derive(Deserialize)]
+pub struct Database {
+	pub database_url: String,
 }
 
 impl Config {
+	pub fn new() -> Result<Self, Error> {
+		let mut config_file = File::open("./config.toml")?;
+		let mut config = String::new();
+		config_file.read_to_string(&mut config)?;
 
+		Ok(toml::from_str(config.as_str())?)
+	}
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,27 @@
+use std::sync::Arc;
+use std::time::Duration;
+use sqlx::postgres::PgPoolOptions;
+use crate::config::Config;
+
 mod config;
 
-fn main() {
-	println!("Hello, world!");
+type Error = Box<dyn std::error::Error + Send + Sync>;
+
+#[actix_web::main]
+async fn main() -> Result<(), Error> {
+	let config = Arc::new(Config::new()?);
+
+	let pool = PgPoolOptions::new()
+		.max_lifetime(Duration::from_secs(6000))
+		.min_connections(3)
+		.max_connections(10)
+		.connect(config.database.database_url.as_str())
+		.await?;
+
+	// Without giving it arguments it will default to $PROJECT_ROOT/migrations
+	sqlx::migrate!()
+		.run(&pool)
+		.await?;
+
+	Ok(())
 }
